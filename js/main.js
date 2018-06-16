1c01b3bedda0a1cd9dc7bf4751401d550d097ba6
Mourad <mkejji@gmail.com>
2018-06-16 03:42:23 +0200

    Chapter 1: A flying bee in a parallax background !

--- js/main.js
+++ js/main.js
@@ -1,26 +1,52 @@
 // Initalization script
 
-function preload () {
+function preload() {
     this.load.image('sky','image/sky.png');
     this.load.image('bee', 'image/bee.png');
 }
 
 
 function create () {
-    var sky = this.add.image(0, 0, 'sky');
-    sky.width = gameWidth;
-    sky.height = gameHeight;
-    this.physics.startSystem(Phaser.Physics.ARCADE);
-    this.bee = this.add.sprite(gameWidth / 2, gameHeight / 2, 'bee');
+    this.sloppyBee = {
+        sprites: {
+            sky: this.add.image(0, 0, 'sky'),
+            bee: this.add.sprite(gameWidth / 2, gameHeight / 2, 'bee')
+        },
+        enemies: [],
+        flowers: []
+    };
+
+    this.sloppyBee.sprites.bee.fallingStage = 0;
+    this.sloppyBee.sprites.bee.escalatingStage = 0;
+    this.sloppyBee.sprites.sky.width = gameWidth;
+    this.sloppyBee.sprites.sky.height = gameHeight;
+
 }
 
-function update () {
+function render() {
+    // game.debug.pointer(game.input.pointer1);
+}
+
+function update() {
+    if (game.input.pointer1.isDown) {
+        this.sloppyBee.sprites.bee.fallingStage = 0;
+        this.sloppyBee.sprites.bee.escalatingStage++;
+        this.sloppyBee.sprites.bee.y -= Math.log(this.sloppyBee.sprites.bee.escalatingStage);
+    } else {
+        this.sloppyBee.sprites.bee.escalatingStage = 0;
+        this.sloppyBee.sprites.bee.fallingStage++;
+        this.sloppyBee.sprites.bee.y += Math.log(this.sloppyBee.sprites.bee.fallingStage);
+    }
+    if (this.sloppyBee.sprites.bee.y >= (gameHeight - this.sloppyBee.sprites.bee.height / 1.5)) {
+        game.paused = true;
+    }
 }
 
 var mainState = {
     preload: preload,
     create: create,
-    update: update
+    update: update,
+    render: render
 };
 
 var game = new Phaser.Game(gameWidth, gameHeight);
--- js/screen.js
+++ js/screen.js
@@ -1,5 +1,4 @@
 // Ideal width and height for current screen
-/*
 var targetWidth = 720;
 var targetHeight = 480;
 
@@ -8,7 +7,12 @@ var newRatio = (targetHeight / targetWidth) * deviceRatio;
 
 var gameWidth = targetWidth * newRatio;
 var gameHeight = targetHeight;
-*/
+
+// Better ?
 var gameRatio = window.innerWidth/window.innerHeight;
-var gameWidth = 640 * gameRatio;
-var gameHeight = 640;
+gameWidth = 640 * gameRatio;
+gameHeight = 640;
+
+// F*CK That Sh*t
+gameWidth = window.innerWidth;
+gameHeight = window.innerHeight;
