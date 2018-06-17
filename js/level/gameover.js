94406be4d22d2dc2c6b130eab7564e558b81ee83
andrena jean-philippe <andrenajp@gmail.com>
2018-06-17 05:56:07 -0300

    add gamOver menu

--- js/level/gameover.js
+++ js/level/gameover.js
@@ -5,4 +5,14 @@ function gameOver(game) {
 	game.gameStarted = false;
 
 	/* TODO: Add Text on Screen with Score + Button retry */
+    var styleR = { font: '65px Arial', fill: '#4286f4', align: 'center' };
+    var styleScore = { font: '65px Arial', fill: '#ff0044', align: 'center' };
+    var finalScore = game.add.text(gameWidth/2-gameWidth*0.25,gameHeight/2 , 'Score: ' + game.sloppyBee.score, styleScore);
+    var buttonR = game.add.text(gameWidth/2+10,gameHeight/2 , 'Rejouer', styleR);
+    buttonR.inputEnabled = true;
+    buttonR.events.onInputDown.add(beeRestart, this);
+}
+
+function beeRestart () {
+    game.state.start(game.state.current);
 }
