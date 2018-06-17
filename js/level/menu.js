7fbb5b77a6f2ce6d6c24c339c5395fc7050c396f
andrena jean-philippe <andrenajp@gmail.com>
2018-06-17 11:17:11 -0300

    add cloud

--- js/level/menu.js
+++ js/level/menu.js
@@ -2,6 +2,7 @@ var mainMenu = {
 	preload: function(game) {
 
 		this.load.image('vent','image/vent.png');
+        this.load.image('jouer','image/jouer.png');
 		this.load.image('fleur','image/solfleuri.png');
 		this.load.image('unicorn','image/unicorn.png');
 		this.load.spritesheet('bee','image/bee.png', 48, 34, 3);
@@ -18,14 +19,14 @@ var mainMenu = {
 				unicorn: this.add.sprite(gameWidth/2-gameWidth*0.3, gameHeight / 2, 'unicorn'),
 				bee: this.add.sprite(gameWidth/2-gameWidth*0.3, gameHeight / 2, 'bee'),
 				fleur: this.add.tileSprite(0, gameHeight - 107, gameWidth, 107, 'fleur'),
+                
 				//            bebe : this.add.sprite(0, 0, 'bebe'),
+                
 				vent: this.add.tileSprite(0, 0, gameWidth, gameHeight, 'vent'),
+                jouer: this.add.sprite(gameWidth/2-gameWidth*0.3, gameHeight / 2 -100, 'jouer')
 			}
 		};
-		var text = 'Jouer';
-		var style = { font: '65px Arial', fill: '#ff0044', align: 'center' };
-		var t = game.add.text(gameWidth/2-60, 0, text, style);
-		t.inputEnabled = true;
+		var t = this.imagMenu.sprites.Jouer.inputEnabled = true;
 		t.events.onInputDown.add(laucher, this);
 	},
 	update: function(game) {
